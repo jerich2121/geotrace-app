2667616827b20b7960766f83d9aabf1684d0bb20
Jericho Marcelo <jerichovincent20@gmail.com>
2026-02-20 02:27:47 +0800

    fixed

--- backend/api/auth/login.js
+++ backend/api/auth/login.js
@@ -1,7 +1,12 @@
-const { login } = require('../../src/controllers/authController');
+const { getHistory, addHistory, deleteHistory } = require('../../src/controllers/historyController');
+const { authenticateToken } = require('../../src/middleware/auth');
 const allowCors = require('../_cors');
 
 module.exports = allowCors((req, res) => {
-  if (req.method !== 'POST') return res.status(405).json({ message: 'Method not allowed' });
-  return login(req, res);
+  return authenticateToken(req, res, () => {
+    if (req.method === 'GET') return getHistory(req, res);
+    if (req.method === 'POST') return addHistory(req, res);
+    if (req.method === 'DELETE') return deleteHistory(req, res);
+    return res.status(405).json({ message: 'Method not allowed' });
+  });
 });
---
+++ backend/api/auth/me.js
@@ -0,0 +1,8 @@
+const { getMe } = require('../../src/controllers/authController');
+const { authenticateToken } = require('../../src/middleware/auth');
+const allowCors = require('../_cors');
+
+module.exports = allowCors((req, res) => {
+  if (req.method !== 'GET') return res.status(405).json({ message: 'Method not allowed' });
+  return authenticateToken(req, res, () => getMe(req, res));
+});
